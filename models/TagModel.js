5da25c66864633ab8a5334179214d8b9c6a7072a
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-24 22:01:11 +1000

    fixed bug with tags

--- models/TagModel.js
+++ models/TagModel.js
@@ -5,7 +5,7 @@ const mongoosePaginate = require('mongoose-paginate-v2')
 const tagSchema = mongoose.Schema({
     label: String,
     confidence: Number,
-    imageId: [{ type: Schema.Types.ObjectId, ref: 'Photo' }],
+    imageId: { type: Schema.Types.ObjectId, ref: 'Photo' },
     source: Object,
     dateAdded: Date
 })
