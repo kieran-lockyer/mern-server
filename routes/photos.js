39808824f0ecb82db9edd78af52b7fd3f324544d
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-24 11:20:08 +1000

    added individual photo route

--- routes/photos.js
+++ routes/photos.js
@@ -17,6 +17,16 @@ router.get('/', (req, res) => {
     )
 })
 
+router.get("/:_id", (req, res) => {
+    Photos.find({ _id: req.params._id })
+        .then(photo => res.json(photo))
+        .catch(error =>
+            res.status(500).json({
+                error: error.message
+            })
+        );
+});
+
 router.delete('/:id', (req, res) => {
     Photos.findByIdAndRemove(req.params.id).then(
         () => res.send(204)
