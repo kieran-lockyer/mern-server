46ee0e3804304c1f27a80b3550740bc5b14c22f7
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-24 14:52:55 +1000

    changed schemas to fit new data models, and changed routes

--- routes/tags.js
+++ routes/tags.js
@@ -14,37 +14,37 @@ router.get("/", (req, res) => {
       res.status(500).json({
         error: error.message
       })
-    );
-});
+    )
+})
 
-router.get("/:tag", (req, res) => {
-  Tags.find({ tag: req.params.tag })
+router.get("/:_id", (req, res) => {
+  Tags.find({ _id: req.params._id })
     .then(tag => res.json(tag))
     .catch(error =>
       res.status(500).json({
         error: error.message
       })
-    );
-});
+    )
+})
 
-router.get("/:tag/images", (req, res) => {
-  Photos.find({ "metadata.tag": req.params.tag })
-    .then(tag => res.json(tag))
+router.get("/images/:tag", (req, res) => {
+  Tags.find({ "label": req.params.tag })
+    .then(photos => res.json(photos))
     .catch(error =>
       res.status(500).json({
         error: error.message
       })
-    );
-});
+    )
+})
 
-router.delete("/:id", (req, res) => {
-  Tags.findByIdAndRemove(req.params.id)
+router.delete("/:_id", (req, res) => {
+  Tags.findByIdAndRemove(req.params._id)
     .then(() => res.send(204))
     .catch(error =>
       res.status(500).json({
         error: error.message
       })
-    );
-});
+    )
+})
 
 module.exports = router;
