19c0adb43fa9d13736b9fdc46753d800193e6fc0
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-31 22:24:22 +1000

    fixing untracked files

--- routes/photos/graphStatsPhotos.js
+++ routes/photos/graphStatsPhotos.js
@@ -1,30 +1,56 @@
-const Photos = require('../../models/PhotoModel')
+const Photos = require("../../models/PhotoModel")
+const moment = require('moment')
 
 module.exports = async (req, res) => {
-    const result = []
-    for (let n = req.params.n; n > 0; n--) {
-        console.log(n)
-        await Photos.find(
+    await Photos.aggregate(
+        [
             {
-                $and: [
-                    {
-                        dateAdded:
-                        {
-                            $lte:
-                                new Date((new Date().getTime() - (n * 24 * 60 * 60 * 1000)))
-                        }
+                $match: {
+                    dateAdded: {
+                        $gte:
+                            new Date(new Date().getTime() - (req.params.n * 24 * 60 * 60 * 1000))
+                    }
+                }
+            },
+            {
+                $project: {
+                    year: { $year: "$dateAdded" },
+                    month: { $month: "$dateAdded" },
+                    dayOfMonth: { $dayOfMonth: "$dateAdded" }
+                }
+            },
+            {
+                $group: {
+                    _id: {
+                        year: '$year',
+                        month: '$month',
+                        dayOfMonth: '$dayOfMonth'
                     },
-                    {
-                        dateAdded:
-                        {
-                            $gte:
-                                new Date((new Date().getTime() - ((n + 1) * 24 * 60 * 60 * 1000)))
-                        }
-                    }]
-            }).then(res => {
-                console.log(res)
-                result.push(res.length)
-            }).catch(err => console.log('NOPE', err))
-    }
-    res.send(result)
+                    count: {
+                        $sum: 1
+                    }
+                }
+            }]
+    ).then(dates => {
+        console.log(dates)
+        let result = []
+        for (let day = req.params.n - 1; day >= 0; day--) {
+            let checkDate = moment().subtract(day, 'days').format('L').split('/')
+            console.log(checkDate)
+            let dateFound = false
+            for (let date of dates) {
+                if (date._id.month === parseInt(checkDate[0]) &&
+                    date._id.dayOfMonth === parseInt(checkDate[1]) &&
+                    date._id.year === parseInt(checkDate[2])) {
+                    dateFound = true
+                    result.push(date.count)
+                }
+            }
+            if (!dateFound) {
+                result.push(0)
+            }
+        }
+        console.log(result)
+        res.send(result)
+    }).catch(err => console.log('NOPE', err))
 }
